fd8701a8c2cb72ad9917bee444b615fe4ebf950f
OrionMat <36226295+OrionMat@users.noreply.github.com>
2018-12-28 14:45:49 +0000

    analysis on each paragraph

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -7,16 +7,15 @@ from matplotlib.ticker import MaxNLocator
 
 # input: paragraph (string) 
 # output: average sentiment of the paragraph and a list containing a tuple of each sentence with its corresponding sentiment
-# sentiment is rating between -5 (VERY negative) and 5 (VERY positive)
-def paragraphSentiment(paragraph):
+# sentiment is rating between -4 (VERY negative) and 4 (VERY positive)
+def calcSentiment(text):
     analyzer = SentimentIntensityAnalyzer()
-    sentence_list = nltk.sent_tokenize(paragraph)
+    sentence_list = nltk.sent_tokenize(text)
     sentiment_list = []
     sentence_Sentiments = 0.0
     for sentence in sentence_list:
             vs = analyzer.polarity_scores(sentence)
             compound_sentiment = round(vs["compound"]*4, 4)
-            #print("{:-<100} {}".format(sentence, str(compound_sentiment)))
             sentiment_list = sentiment_list + [compound_sentiment]
             sentence_Sentiments += compound_sentiment
 
@@ -27,21 +26,25 @@ def paragraphSentiment(paragraph):
 
 # article file to a string
 article = open("article.txt", "r")
-text = article.read()
+article_text = article.read()
 
 # string split into paragraphs
-paragraph_list = text.split("\n")
+paragraph_list = article_text.split("\n")
 paragraph_list = list(filter(None, paragraph_list))
 
 
 # paragraphs sentiment analysed
-#para_sentiments_list = []
-#for paragraph in paragraph_list:
-    #para_sentiment, modified_paragraph = paragraphSentiment(paragraph)
-    #para_sentiments_list = para_sentiments_list + para_sentiment
-para_sentiment, modified_paragraph = paragraphSentiment(text)
-print(text, "\n\n", modified_paragraph, "\n\n", "average sentiment: " + str(para_sentiment))
+para_sentiments_list = []
+modified_paragraph_list = []
+for paragraph in paragraph_list:
+    para_sentiment, modified_paragraph = calcSentiment(paragraph)
+    para_sentiments_list = para_sentiments_list + [para_sentiment]
+    modified_paragraph_list = modified_paragraph_list + [modified_paragraph]
 
+# total sentiment (from all sentences)
+sentiment_tot, modified_tot = calcSentiment(article_text)
+
+'''
 sentence_list, sentiment_list = zip(*modified_paragraph)
 
 print("hi")
@@ -51,3 +54,4 @@ plt.plot(np.arange(1, len(sentiment_list)+1), sentiment_list, '-s')
 plt.xlabel('Sentence index')
 plt.ylabel('Sentiment Intensity')
 plt.show()
+'''
