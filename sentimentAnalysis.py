ef7bfaf9b9a0398e642460ca09a5c14f34a55146
OrionMat <36226295+OrionMat@users.noreply.github.com>
2018-12-28 22:32:18 +0000

    added real news analysis. plots each articles sentiment

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -83,7 +83,45 @@ for file_name in files:
         if exc.errno != errno.EISDIR:
             raise
 
+plt.plot(np.arange(1, len(article_sentiment_tot_list)+1), article_sentiment_tot_list, '-bo')
 
+# loops though all the fact articles
+path = 'C:\\Users\\orion\\Documents\\Python programming\\Sentiment Analysis\\FakeNewsNet-master\\Data\\BuzzFeed\\RealNewsContent\\*.json'
+files = glob.glob(path)
+
+article_sentiment_tot_list = []
+for file_name in files:
+    try:
+        with open(file_name, 'r') as json_file:
+            json_string = json_file.read()
+            #print(json_string)
+            json_dict = json.loads(json_string)
+            article_text = json_dict["text"]
+            #print(json_dict["text"])
+
+            # string split into paragraphs
+            paragraph_list = article_text.split("\n")
+            paragraph_list = [paragraph.strip(' ') for paragraph in paragraph_list]
+            paragraph_list = list(filter(None, paragraph_list))
+            #print(paragraph_list)
+
+            # paragraphs sentiment analysed
+            
+            para_sentiments_list = []
+            modified_paragraph_list = []
+            for paragraph in paragraph_list:
+                para_sentiment, modified_paragraph = calcSentiment(paragraph)
+                para_sentiments_list = para_sentiments_list + [para_sentiment]
+                modified_paragraph_list = modified_paragraph_list + [modified_paragraph]
+
+            # total sentiment (from all sentences)
+            article_sentiment_tot, modified_tot = calcSentiment(article_text)
+            article_sentiment_tot_list = article_sentiment_tot_list + [article_sentiment_tot]
+            
+
+    except IOError as exc:
+        if exc.errno != errno.EISDIR:
+            raise
 
 
 '''
@@ -107,7 +145,7 @@ plt.show()
 '''
 
 # plot of sentiment for each article
-plt.figure()
+#plt.figure()
 plt.plot(np.arange(1, len(article_sentiment_tot_list)+1), article_sentiment_tot_list, '-rs')
 plt.xlabel('Paragraph index')
 plt.ylabel('Average Sentiment Intensity')
