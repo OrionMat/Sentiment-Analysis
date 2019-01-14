563a8d020035491d60a69b1dddc533335c26eefe
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-01-14 22:34:46 +0000

    added BBC sentiment analysis

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -82,6 +82,19 @@ def politi_buzz_analysis(path):
                 raise
     return article_list_analysis(article_list)
 
+def BBC_analysis(path):
+    files = glob.glob(path)
+    article_list = []
+    for file_name in files:
+        try:
+            with open(file_name, 'r') as txt_file:
+                article_text = txt_file.read()
+                article_list = article_list + [article_text]   
+        except IOError as exc:
+            if exc.errno != errno.EISDIR:
+                raise
+    return article_list_analysis(article_list)
+
 def kaggel_Fact_Fake_analysis(path):
     df = pandas.read_csv(path)
     fake_df = df.loc[df['label'] == 1]
@@ -134,14 +147,29 @@ kagg_fake_article_sentiments, kagg_fake_sentences, kagg_fake_sentence_sentiments
 #kagg_fake_news_path = 'News_Data\\fake-news-kaggle\\fake.csv'
 #kagg_fake_article_sentiments, kagg_fake_sentence_sentiments = kaggel_Fake_analysis(kagg_fake_news_path)
 
+# BBC data set
+BBC_busin_news_path = 'News_Data\\bbc-articles\\bbc\\business\\*.txt'
+BBC_enter_news_path = 'News_Data\\bbc-articles\\bbc\\entertainment\\*.txt'
+BBC_polit_news_path = 'News_Data\\bbc-articles\\bbc\\politics\\*.txt'
+BBC_sport_news_path = 'News_Data\\bbc-articles\\bbc\\sport\\*.txt'
+BBC_techn_news_path = 'News_Data\\bbc-articles\\bbc\\tech\\*.txt'
+BBC_busin_sentences, BBC_busin_sentence_sentiments, BBC_busin_article_sentiments  = BBC_analysis(BBC_busin_news_path)
+BBC_enter_sentences, BBC_enter_sentence_sentiments, BBC_enter_article_sentiments  = BBC_analysis(BBC_enter_news_path)
+BBC_polit_sentences, BBC_polit_sentence_sentiments, BBC_polit_article_sentiments  = BBC_analysis(BBC_polit_news_path)
+BBC_sport_sentences, BBC_sport_sentence_sentiments, BBC_sport_article_sentiments  = BBC_analysis(BBC_sport_news_path)
+BBC_techn_sentences, BBC_techn_sentence_sentiments, BBC_techn_article_sentiments  = BBC_analysis(BBC_techn_news_path)
+
 buzz_fake_flat_list = [item for sublist in buzz_fake_sentence_sentiments for item in sublist]
 buzz_real_flat_list = [item for sublist in buzz_real_sentence_sentiments for item in sublist]
 poli_fake_flat_list = [item for sublist in poli_fake_sentence_sentiments for item in sublist]
 poli_real_flat_list = [item for sublist in poli_real_sentence_sentiments for item in sublist]
 kagg_fake_flat_list = [item for sublist in kagg_fake_sentence_sentiments for item in sublist]
 kagg_real_flat_list = [item for sublist in kagg_real_sentence_sentiments for item in sublist]
-
-
+BBC_busin_flat_list = [item for sublist in BBC_busin_sentence_sentiments for item in sublist]
+BBC_enter_flat_list = [item for sublist in BBC_enter_sentence_sentiments for item in sublist]
+BBC_polit_flat_list = [item for sublist in BBC_polit_sentence_sentiments for item in sublist]
+BBC_sport_flat_list = [item for sublist in BBC_sport_sentence_sentiments for item in sublist]
+BBC_techn_flat_list = [item for sublist in BBC_techn_sentence_sentiments for item in sublist]
 
 #%%
 # mean and varience calculations
@@ -151,6 +179,11 @@ poli_fake_article_sentiments, fake_art_avg_poli, fake_art_var_poli = avg_var_cal
 poli_real_article_sentiments, real_art_avg_poli, real_art_var_poli = avg_var_calculation(poli_real_article_sentiments, "real", "(poli)")
 kagg_fake_article_sentiments, fake_art_avg_kagg, fake_art_var_kagg = avg_var_calculation(kagg_fake_article_sentiments, "fake", "(kagg)")
 kagg_real_article_sentiments, real_art_avg_kagg, real_art_var_kagg = avg_var_calculation(kagg_real_article_sentiments, "real", "(kagg)")
+BBC_busin_article_sentiments, busin_art_avg_BBC, busin_art_var_BBC = avg_var_calculation(BBC_busin_article_sentiments, "business", "(BBC)")
+BBC_enter_article_sentiments, enter_art_avg_BBC, enter_art_var_BBC = avg_var_calculation(BBC_enter_article_sentiments, "entertainment", "(BBC)")
+BBC_polit_article_sentiments, polit_art_avg_BBC, polit_art_var_BBC = avg_var_calculation(BBC_polit_article_sentiments, "politics", "(BBC)")
+BBC_sport_article_sentiments, sport_art_avg_BBC, sport_art_var_BBC = avg_var_calculation(BBC_sport_article_sentiments, "sports", "(BBC)")
+BBC_techn_article_sentiments, techn_art_avg_BBC, techn_art_var_BBC = avg_var_calculation(BBC_techn_article_sentiments, "technology", "(BBC)")
 
 
 
@@ -188,6 +221,16 @@ plt.plot(np.arange(1, len(poli_real_article_sentiments)+1), poli_real_article_se
 plt.plot(np.arange(1, len(kagg_fake_article_sentiments)+1), kagg_fake_article_sentiments, 'r^')
 plt.plot(np.arange(1, len(kagg_real_article_sentiments)+1), kagg_real_article_sentiments, 'g^')
 plt.title('Article sentiments')
+plt.xlabel('Article index')
+plt.ylabel('Average Sentiment Intensity')
+
+plt.figure("BBC article sentiments")
+plt.plot(np.arange(1, len(BBC_busin_article_sentiments)+1), BBC_busin_article_sentiments, 'r.')
+plt.plot(np.arange(1, len(BBC_enter_article_sentiments)+1), BBC_enter_article_sentiments, 'g.')
+plt.plot(np.arange(1, len(BBC_polit_article_sentiments)+1), BBC_polit_article_sentiments, 'b.')
+plt.plot(np.arange(1, len(BBC_sport_article_sentiments)+1), BBC_sport_article_sentiments, 'y.')
+plt.plot(np.arange(1, len(BBC_techn_article_sentiments)+1), BBC_techn_article_sentiments, 'm.')
+plt.title('BBC article sentiments')
 plt.xlabel('Article index')
 plt.ylabel('Average Sentiment Intensity')
 
@@ -245,6 +288,29 @@ df_news.plot.kde(title='Kaggel KDE (articles)')
 plt.xlabel('Sentiment intencity')
 plt.ylabel('Probability')
 
+plt.figure("BBC articles")
+plt.hist(BBC_busin_article_sentiments, bins=bin_num, color='r', alpha=0.7, rwidth=0.85, label='busin')
+plt.hist(BBC_enter_article_sentiments, bins=bin_num, color='g', alpha=0.7, rwidth=0.85, label='enter')
+plt.hist(BBC_polit_article_sentiments, bins=bin_num, color='b', alpha=0.7, rwidth=0.85, label='polit')
+plt.hist(BBC_sport_article_sentiments, bins=bin_num, color='y', alpha=0.7, rwidth=0.85, label='sport')
+plt.hist(BBC_techn_article_sentiments, bins=bin_num, color='m', alpha=0.7, rwidth=0.85, label='techn')
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Sentiment intencity')
+plt.ylabel('Frequency')
+plt.legend(loc='upper right')
+plt.title('BBC articles')
+
+#plt.figure("BBC KDE: Fact vs. polit")
+df_news_busin = pandas.DataFrame({'busin': BBC_busin_article_sentiments})
+df_news_enter = pandas.DataFrame({'enter': BBC_enter_article_sentiments})
+df_news_polit = pandas.DataFrame({'polit': BBC_polit_article_sentiments})
+df_news_sport = pandas.DataFrame({'sport': BBC_sport_article_sentiments})
+df_news_techn = pandas.DataFrame({'techn': BBC_techn_article_sentiments})
+df_news = pandas.concat([df_news_busin, df_news_enter, df_news_polit, df_news_sport, df_news_techn], axis=1)
+df_news.plot.kde(title='BBC KDE (articles)')
+plt.xlabel('Sentiment intencity')
+plt.ylabel('Probability')
+
 #%%
 
 # histogram plots: (of sentence sentiments)
@@ -297,6 +363,29 @@ df_news_real = pandas.DataFrame({'Real': kagg_real_flat_list})
 df_news_fake = pandas.DataFrame({'Fake': kagg_fake_flat_list})
 df_news = pandas.concat([df_news_real,df_news_fake], axis=1)
 df_news.plot.kde(title='Kaggel KDE (sentences)')
+plt.xlabel('Sentiment intencity')
+plt.ylabel('Probability')
+
+plt.figure("BBC senctence sentiments") # plot of sentiment for each sentence
+plt.hist(BBC_busin_flat_list, bins=bin_num, color='r', alpha=0.7, rwidth=0.85, label='busin')
+plt.hist(BBC_enter_flat_list, bins=bin_num, color='g', alpha=0.7, rwidth=0.85, label='enter')
+plt.hist(BBC_polit_flat_list, bins=bin_num, color='b', alpha=0.7, rwidth=0.85, label='polit')
+plt.hist(BBC_sport_flat_list, bins=bin_num, color='y', alpha=0.7, rwidth=0.85, label='sport')
+plt.hist(BBC_techn_flat_list, bins=bin_num, color='m', alpha=0.7, rwidth=0.85, label='techn')
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Sentiment intencity')
+plt.ylabel('Frequency')
+plt.legend(loc='upper right')
+plt.title('BBC senctence sentiments')
+
+#plt.figure("BBC KDE: Fact vs. polit (sentences)")
+df_news_busin = pandas.DataFrame({'busin': BBC_busin_flat_list})
+df_news_enter = pandas.DataFrame({'enter': BBC_enter_flat_list})
+df_news_polit = pandas.DataFrame({'polit': BBC_polit_flat_list})
+df_news_sport = pandas.DataFrame({'sport': BBC_sport_flat_list})
+df_news_techn = pandas.DataFrame({'techn': BBC_techn_flat_list})
+df_news = pandas.concat([df_news_busin, df_news_enter, df_news_polit, df_news_sport, df_news_techn], axis=1)
+df_news.plot.kde(title='BBC KDE (sentences)')
 plt.xlabel('Sentiment intencity')
 plt.ylabel('Probability')
 
