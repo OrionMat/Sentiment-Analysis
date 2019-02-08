2f83993ca900da3f92db30d6dc13814e1dad6c97
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-02-08 17:30:26 +0000

    added web scraping

--- newsSentimentAnalysis.py
+++ newsSentimentAnalysis.py
@@ -8,6 +8,8 @@ import glob
 import errno
 import pandas
 import random
+import requests
+from bs4 import BeautifulSoup
 
 
 #%%
@@ -134,3 +136,50 @@ def kaggle_mult_news_analysis(path, publication):
 #    art_sub_list = random.sample(article_list, 100)  # needed as 13000 takes too long
 #    sentence_list, sentence_sentiment_list, article_sentiment_tot_list = article_list_analysis(art_sub_list)
 #    return article_sentiment_tot_list, sentence_list, sentence_sentiment_list
+
+
+
+### NEWS WEB SCRAPING ##
+
+
+def get_article(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # get article title
+    #title = soup.find('h1')
+    #title_txt = title.get_text() 
+
+    # get all paragraphs in the article body
+    articlebody = soup.find(attrs={"name": "articleBody"})
+    if articlebody == None: # catches videos
+        return ""
+    all_paragraphs = articlebody.find_all('p')
+
+    # get the text of all paragraphs and flatten them into an article
+    article = ''
+    for paragraph in all_paragraphs:
+        text = paragraph.get_text()
+        article = article + text
+    return(article)
+
+def nyt_scrape(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    article_links = soup.find_all('article') 
+
+    hyp_link_list = []
+    for link in article_links:
+        url = link.find('a')
+        hyperlink = url.get('href')
+        hyp_link_list = hyp_link_list + [hyperlink]
+
+    article_list = []
+    for hyp_link in hyp_link_list:
+        url = 'https://www.nytimes.com'+ hyp_link
+        article = get_article(url)
+        if article:
+            article_list = article_list + [article]
+
+
+    return article_list
