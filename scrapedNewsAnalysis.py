388d1335eccf9feba610541159bef45e5cac9d10
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-02-25 14:36:40 +0000

    added saving articles to CSV file

--- scrapedNewsAnalysis.py
+++ scrapedNewsAnalysis.py
@@ -3,6 +3,61 @@ import newsSentimentAnalysis as NSA
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas
+import csv
+
+
+# initiates csv file and writes headers
+# use when running script for first time 
+# i.e no existing data in csv file
+def initiate_csv(csv_file, csv_headers):
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
+            writer.writeheader()
+    except IOError:
+        print("I/O error") 
+
+def append_csv(csv_file, csv_headers, dict_data):
+    # checks for repeated articles by their titles and agency
+    agency_list = []
+    title_list = []
+    try:
+        with open(csv_file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                agency_list = agency_list + [row['agency']]
+                title_list = title_list + [row['title']]
+    except IOError:
+        print("I/O error") 
+
+    # appends data to csv file
+    try:
+        with open(csv_file, 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_headers)
+            for data in dict_data:
+                if data['title'] not in title_list:
+                    writer.writerow(data)
+                else:
+                    indices = [i for i, x in enumerate(title_list) if x == data['title']]   # gets list of indexes where title is the same
+                    agencies = [agency_list[index] for index in indices]                    # selects the correspoding agencies
+                    for agency in agencies:
+                        # checks if agency is the different 
+                        if data['agency'] != agency:
+                            writer.writerow(data)
+    except IOError:
+        print("I/O error") 
+
+def lists_to_dictList(agency, title_list, article_list):
+    dic_list =[]
+    for idx in range(len(title_list)):
+        dic = {'agency' : agency, 'title' : title_list[idx], 'article' : article_list[idx]}
+        dic_list.append(dic)
+    return dic_list
+
+# %% ANALYSIS + SAVE TO CSV
+
+csv_file_path = 'real_or_opinion.csv'
+csv_columns = ['agency','title','article']
 
 nyt_opinion_url = 'https://www.nytimes.com/section/opinion'
 nyt_world_url = 'https://www.nytimes.com/section/world'
@@ -10,20 +65,19 @@ nyt_world_url = 'https://www.nytimes.com/section/world'
 NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list, NYTscrap_opinion_sentence_sentiments, NYTscrap_opinion_article_sentiments = NSA.nyt_scrape_analysis(nyt_opinion_url)
 NYTscrap_world_title_list, NYTscrap_world_sentence_list, NYTscrap_world_sentence_sentiments, NYTscrap_world_article_sentiments = NSA.nyt_scrape_analysis(nyt_world_url)
 
-#print(NYTscrap_opinion_sentence_list[0], '\n\n')
-#print(NYTscrap_opinion_title_list[0], '\n\n')
-#data = {'Title':title, 
-#        'Author':authorname, 
-#        'PageLink':pagelinks, 
-#        'Article':myarticle, 
-#        'Date':datetime.now()}
-#data = {'Article':myarticle}
+#%%
+news_dicList = lists_to_dictList('NYT opinion', NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list)
+print(news_dicList)
+#news_dictionary = dict(zip(NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list))
 
-#print(len(NYTscrap_opinion_sentence_list), "  ", len(NYTscrap_opinion_title_list))
+initiate_csv(csv_file_path, csv_columns)                    # use first time 
+append_csv(csv_file_path, csv_columns, news_dicList)     # use rest of the time
 
-dictionary = dict(zip(NYTscrap_opinion_title_list, NYTscrap_opinion_sentence_list))
-print(dictionary)
-# %%
+
+
+
+
+
 
 NYTscrap_opinion_flat_list = [item for sublist in NYTscrap_opinion_sentence_sentiments for item in sublist]
 NYTscrap_world_flat_list = [item for sublist in NYTscrap_world_sentence_sentiments for item in sublist]
@@ -38,16 +92,9 @@ df_news_NYTscrap_opinion_article_avg = pandas.DataFrame({'NYT opinion articles':
 df_news_NYTscrap_world_article_avg = pandas.DataFrame({'NYT world news articles': NYTscrap_world_article_sentiments})
 df_news_NYTscrap_article_avg = pandas.concat([df_news_NYTscrap_opinion_article_avg, df_news_NYTscrap_world_article_avg], axis=1)
 
-
 df_news_NYTscrap_opinion_article_abs = pandas.DataFrame({'NYT opinion articles': NYTscrap_opinion_art_abs})
 df_news_NYTscrap_world_article_abs = pandas.DataFrame({'NYT world news articles': NYTscrap_world_art_abs})
 df_news_NYTscrap_article_abs = pandas.concat([df_news_NYTscrap_opinion_article_abs, df_news_NYTscrap_world_article_abs], axis=1)
-
-
-
-
-
-
 
 df_news_NYTscrap_opinion_sentence_avg = pandas.DataFrame({'NYTscrap_opinion': NYTscrap_opinion_flat_list})
 df_news_NYTscrap_opinion_sentence_abs = pandas.DataFrame({'NYTscrap_opinion': np.absolute(np.asarray(NYTscrap_opinion_flat_list))})
