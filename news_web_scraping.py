8f5a658b9e7f94988b20de528d9c71781072626e
OrionMat <36226295+OrionMat@users.noreply.github.com>
2019-03-15 20:58:37 +0000

    added AP and check for no results

--- news_web_scraping.py
+++ news_web_scraping.py
@@ -12,6 +12,7 @@ import data_saving as save_to_CSV
 
 query = "Ethiopian Airlines plane crashed"
 # Shares in Boeing fell by 12.9% on Monday in the wake of the crash
+# Can Trump declare a national emergency to build a wall?
 
 
 
@@ -97,7 +98,7 @@ def NYT_page_scrape(url):
 
 #%% BBC:
 
-# gets BBC links from google with article dates
+# gets BBC links from google
 def google_BBC_links(query):
     BBC_query = "BBC news + " + query
     article_links = search(BBC_query, tld="co.uk", num=15, stop=10, pause=2)    # gets 15 top links from google
@@ -151,7 +152,60 @@ def BBC_article_scrape(url):
     date = date_location.get_text()
     return headline, article, date
 
+#%% AP
 
+# gets AP links from google
+def google_AP_links(query):
+    AP_query = "AP news + " + query
+    article_links = search(AP_query, tld="co.uk", num=15, stop=10, pause=2)    # gets 15 top links from google
+    url_list = []
+    for link in article_links:
+        if (re.search(r"www.apnews.com", link) != None) and  (re.search(r"gallery", link) == None) :
+            url_list = url_list + [link]
+    return url_list
+
+# gets a list of AP articles and their corresponing headlines and dates from a list of links
+def AP_links_scrape(link_list):
+    title_list = []
+    article_list = []
+    date_list = []
+    for link in link_list:
+        title, article, date = AP_article_scrape(link)
+        if title and article and date:
+            title_list = title_list + [title]
+            article_list = article_list + [article]
+            date_list = date_list + [date]
+    return title_list, article_list, date_list
+
+# scrapes a AP article, its headline and its date
+def AP_article_scrape(url):
+    # get webpage
+    try:
+        response = requests.get(url)
+    except:
+        print("no response from webpage. Error: ", sys.exc_info()[0])
+        raise
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # get article title
+    title = soup.find('h1')
+    if title == None:
+        print("AP article title not found")
+        return "", "", ""
+    headline = title.get_text() 
+    # get all paragraphs in the article body
+    articlebody = soup.find(attrs={"data-key": "article"})
+    if articlebody == None: # catches videos
+        return "","", ""
+    all_paragraphs = articlebody.find_all('p')
+    # get the text of all paragraphs and flatten them into an article
+    article = ''
+    for paragraph in all_paragraphs:
+        text = paragraph.get_text()
+        article = article + text
+    # get article date
+    date_location = soup.find(attrs={"data-key": "timestamp"})
+    date = date_location.get_text()
+    return headline, article, date
 
 
 
@@ -167,8 +221,9 @@ title_list, article_list = NYT_links_scrape(url_list)
 #for idx in range(len(title_list)):
 #    print(date_list[idx], "  :  ", title_list[idx], "  :  ", url_list[idx])
 #    print(article_list[idx][0:50], '\n')
-news_dicList = save_to_CSV.lists_to_dictList('NYT', title_list, date_list, article_list, url_list)
-save_to_CSV.append_csv(csv_file_path, csv_columns, news_dicList)     # use rest of the time
+if title_list and date_list and article_list and url_list:
+    news_dicList = save_to_CSV.lists_to_dictList('NYT', title_list, date_list, article_list, url_list)
+    save_to_CSV.append_csv(csv_file_path, csv_columns, news_dicList)   
 
 # BBC:
 url_list = google_BBC_links(query)
@@ -176,7 +231,16 @@ title_list, article_list, date_list = BBC_links_scrape(url_list)
 #for idx in range(len(title_list)):
 #    print(date_list[idx], "  :  ", title_list[idx], "  :  ", url_list[idx])
 #    print(article_list[idx][0:50], '\n')
-news_dicList = save_to_CSV.lists_to_dictList('BBC', title_list, date_list, article_list, url_list)
-csv_file_path = 'consensus_data.csv'
-csv_columns = ['agency', 'title', 'date', 'article', 'link']
-save_to_CSV.append_csv(csv_file_path, csv_columns, news_dicList)     # use rest of the time
+if title_list and date_list and article_list and url_list:
+    news_dicList = save_to_CSV.lists_to_dictList('BBC', title_list, date_list, article_list, url_list)
+    save_to_CSV.append_csv(csv_file_path, csv_columns, news_dicList)   
+
+# AP:
+url_list = google_AP_links(query)
+title_list, article_list, date_list = AP_links_scrape(url_list)
+#for idx in range(len(title_list)):
+#    print(date_list[idx], "  :  ", title_list[idx], "  :  ", url_list[idx])
+#    print(article_list[idx][0:50], '\n')
+if title_list and date_list and article_list and url_list:
+    news_dicList = save_to_CSV.lists_to_dictList('AP', title_list, date_list, article_list, url_list)
+    save_to_CSV.append_csv(csv_file_path, csv_columns, news_dicList)   
